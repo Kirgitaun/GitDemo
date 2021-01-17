91b6b769047f6aaf3cf1e56a9f91597984789662
Alexander Kirgitaun <kirgitaun@mail.ru>
2021-01-18 00:25:09 +0900

    Initial Commit

---
+++ GitDemo2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo2
+//
+//  Created by Alex Alexandro on 18.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
